485008abad7bb85581e8f6bb2a217d043ac82ad9
Himanshu0705-coder <panchalhimanshu0507@gmail.com>
2026-09-02 16:41:40 +0530

    datatype

---
+++ 01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 12345
+let accountEmail = "himanshu@gmail.com"
+var accountPassword = "12345"
+
+
+accountEmail = "Hima@gmail.com"
+
+
+console.log(accountId)
+console.log(accountEmail)
+
+console.table([accountId,accountEmail,accountPassword])
+
+
+
+// "const" cannot be changed 
+// "let" uses to declare a value
+// we caanot use "var" because sometime it creates problem 
+/* 
+
+
+*/
---
+++ 02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";   // treat all JS code as newer version
+
+
+// alert("hello")   // we are using nodejs , not browser
+
+console.log(3 + 3);   // using console.log instead of alert for Node.js
+
+// code readability should be high
+
+// let name = "Himanshu Panchal"
+// let age = 21
+// let isLogged = Yes
+
+
+// number => 2 to power 52
+// bigint
+// string => ""
+// boolena => true/false
+// null => standalone value
+// undefine 
+// symbol=> unique
+
+
+// object
+
+console.log(typeof "Himanshu")
+console.log(typeof 21)
+console.log(typeof true)
+console.log(typeof undefined)   // undefine
+console.log(typeof null)   // object

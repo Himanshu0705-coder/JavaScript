a7a24a37d4f1c436ac8397742eef8371f883315a
Himanshu0705-coder <panchalhimanshu0507@gmail.com>
2026-09-02 18:36:56 +0530

    new

---
+++ 04_Operations.js
@@ -0,0 +1,22 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(10/2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2+3*5);   // 17
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+let gameCounter = 100
+// gameCounter++
+// ++gameCounter
+console.log(gameCounter);

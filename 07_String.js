b2749d18bc3a464ef85989d4f929bb2bc131541b
Himanshu0705-coder <panchalhimanshu0507@gmail.com>
2026-09-05 11:25:24 +0530

    05_09_2026

---
+++ 07_String.js
@@ -0,0 +1,39 @@
+
+// concatenate two values
+const name = "Himanshu"
+const repoCount = 50
+
+ console.log(name + repoCount )
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+const gameName = new String('Himanshu-pan')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('a'))
+
+const newString = gameName.substring(0,4)
+// console.log(newString)
+
+const anotherString = gameName.slice(0,4)
+// console.log(anotherString)
+
+const newStringOne = "  Himanshu      "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+
+const url = "https://himanshu.com/himanshu%20panchal"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('data'))    // gives true false values
+
+// console.log(url.split('.'))

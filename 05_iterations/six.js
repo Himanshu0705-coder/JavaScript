5260edb398382a5122bb50ef0e6d13fdcb76c687
Himanshu0705-coder <panchalhimanshu0507@gmail.com>
2026-09-17 02:10:37 +0530

    17.09.2026

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,1 +1,31 @@
-coding = ["js","ruby","java","python","cpp"]
+// coding = ["js","ruby","java","python","cpp"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item)
+// }  )
+
+// console.log(values)
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+
+// // console.log(myNums)
+// console.log(newNums)
+
+
+
+
+ 
+const newNums = []
+
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+})
+console.log(newNums)
+
